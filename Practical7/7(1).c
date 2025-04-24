5330df7bd35afc8d5cb2d68fde378a75fdb2732e
devpatel09092006 <24012011080@gnu.ac.in>
2025-04-24 17:14:28 +0530

    New

--- Practical7/7(1).c
+++ Practical7/7(1).c
@@ -1,33 +1,36 @@
 #include <stdio.h>
 
-
-// function Prototype
 int isPrime(int num);
 
 int main () {
-    
-    int num;
-    
-    printf("Enter a number: ");
-    scanf("%d", &num);
-    
-    
-    // Function Calling
-    if (isPrime(num)) {
-        printf("%d is a Prime number.\n", num);
-    }else {
-        printf("%d is not a Prime number.\n", num);
-    }
-    
-    return 0;
+
+   int number;
+
+   printf("Enter a number: ");
+   scanf("%d", &number);
+
+   if (isPrime(number)) {
+        printf("Prime number.\n");
+   } else {
+        printf("Not a prime number.\n");
+   }
+
 }
 
-// function Definition
-int isPrime (int num) {
-    if (num < 2) return 0;
-    for (int i = 2; i * i <= num; i++) {
-        if (num % i == 0)
-             return 0;
+
+int isPrime(int num) {
+
+    int i;
+
+    if (num <= 1) {
+        return 0;
     }
+
+    for (i = 2; i <= num / 2; i++) {
+        if (num % i == 0) {
+            return 0;
+        }
+    }
+
     return 1;
 }
