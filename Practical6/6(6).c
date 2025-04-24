e5f4a7edc8b506902fc972b0f8ae4791e1f2031a
devpatel09092006 <24012011080@gnu.ac.in>
2025-04-24 16:41:29 +0530

    Create 6(6).c

---
+++ Practical6/6(6).c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main () {
+
+   int a[3][3], b[3][3], mult[3][3], i, j, k;
+
+   printf("Enter elements of first 3x3 matrix:\n");
+    for (i = 0; i < 3; i++) {
+        for (j = 0; j < 3; j++) {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    printf("Enter elements of second 3x3 matrix:\n");
+    for (i = 0; i < 3; i++) {
+        for (j = 0; j < 3; j++) {
+            scanf("%d", &b[i][j]);
+        }
+    }
+
+    for (i = 0; i < 3; i++) {
+        for (j = 0; j < 3; j++) {
+            mult[i][j] = 0;
+            for (k = 0; k < 3; k++) {
+                mult[i][j] += a[i][k] * b[k][j];
+            }
+        }
+    }
+
+    printf("Multiplication of the matrices:\n");
+    for (i = 0; i < 3; i++) {
+        for (j = 0; j < 3; j++) {
+            printf("%d", mult[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
