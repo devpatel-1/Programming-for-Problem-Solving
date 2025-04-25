11e3f613db4355370c59d4e3cc427721d7a16997
devpatel09092006 <24012011080@gnu.ac.in>
2025-04-25 09:23:39 +0530

    Update practical7(2(3)).c

--- Practical7/practical7(2(3)).c
+++ Practical7/practical7(2(3)).c
@@ -1,22 +1,20 @@
 // Code 3 :- With Arguments but no Return Value 
 
-
 #include <stdio.h>
-#define PI 3.14
 
-void areaOfCircle (float radius) {
-    
-    float area = PI * radius * radius;
-    printf("Area : %.2f\n", area);
-    
-} 
+void area(float r);
 
 int main () {
-    
-    float r;
-    printf("Enter the radius: ");
-    scanf("%f", &r);
-    areaOfCircle(r);
+
+   float radius;
+   printf("Enter radius of circle: ");
+   scanf("%f", &radius);
+   area(radius);
     return 0;
-    
+
+}
+
+void area (float r) {
+    float result = 3.14 * r * r;
+    printf("Area of circle: %.2f\n", result);
 }
