61adc96dbadf0fff061b6ad68e05e03e688e8d05
devpatel09092006 <24012011080@gnu.ac.in>
2025-04-26 09:35:00 +0530

    Create 6(5).c

---
+++ Practical6/6(5).c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main () {
+
+   int a[2][2], i, j;
+
+   printf("Enter elements of 2x2 matrix:\n");
+   for (i = 0; i < 2; i++) {
+      for (j = 0; j < 2; j++) {
+         scanf("%d", &a[i][j]);
+      }
+   }
+
+   printf("Transpose of the matrix:\n");
+   for (i = 0; i < 2; i++) {
+      for (j = 0; j < 2; j++) {
+         printf("%d ", a[j][i]);
+      }
+      printf("\n");
+   }
+   return 0;
+
+}
