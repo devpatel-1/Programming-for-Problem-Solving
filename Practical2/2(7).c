00f3dd340d2429d952f79da63f4393fea0d13a6d
devpatel09092006 <24012011080@gnu.ac.in>
2025-05-21 17:19:26 +0530

    Update 2(7).c

--- Practical2/2(7).c
+++ Practical2/2(7).c
@@ -5,30 +5,28 @@
 
 
 #include <stdio.h>
+#define PI 3.14159265358979323846
 
-#define PI 3.14159  // Define the value of Pi
+int main () {
 
-int main() {
     double radius, height, length, width;
-    double volume_cylinder, volume_sphere, volume_cuboid;
+    double volumeCylinder, volumeSphere, volumeCuboid;
 
-    // Calculate Volume of Cylinder (V = πr²h)
-    printf("Enter radius and height of the Cylinder: ");
+    printf("Enter the radius and height of the cylinder: ");
     scanf("%lf %lf", &radius, &height);
-    volume_cylinder = PI * radius * radius * height;
-    printf("Volume of Cylinder: %.2lf\n\n", volume_cylinder);
+    volumeCylinder = PI * radius * radius * height;
+    printf("Volume of Cylinder: %.2lf\n", volumeCylinder);
 
-    // Calculate Volume of Sphere (V = (4/3)πr³)
-    printf("Enter radius of the Sphere: ");
+    printf("Enter the radius of the sphere: ");
     scanf("%lf", &radius);
-    volume_sphere = (4.0 / 3.0) * PI * radius * radius * radius;
-    printf("Volume of Sphere: %.2lf\n\n", volume_sphere);
+    volumeSphere = (4.0 / 3.0) * PI * radius * radius * radius;
+    printf("Volume of Sphere: %.2lf\n", volumeSphere);
 
-    // Calculate Volume of Cuboid (V = l × w × h)
-    printf("Enter length, width, and height of the Cuboid: ");
-    scanf("%lf %lf %lf", &length, &width, &height);
-    volume_cuboid = length * width * height;
-    printf("Volume of Cuboid: %.2lf\n", volume_cuboid);
+    printf("Enter the length and width of the cuboid: ");
+    scanf("%lf %lf", &length, &width);
+    volumeCuboid = length * width * height; // Assuming height is the same as for the cylinder
+    printf("Volume of Cuboid: %.2lf\n", volumeCuboid);
 
     return 0;
+
 }
