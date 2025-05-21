bd4e4b584701431e0feff21d61d0bf929fa8e601
devpatel09092006 <24012011080@gnu.ac.in>
2025-05-21 17:19:31 +0530

    Update 3(1).c

--- Practical3/3(1).c
+++ Practical3/3(1).c
@@ -15,7 +15,7 @@ int main() {
 
   int a, b, c, d;
 
-  printf("Enter the value of a, b , c, and d : ");
+  printf("Enter four integers (a, b, c, d): ");
   scanf("%d %d %d %d", &a, &b, &c, &d);
 
   int res1 = a + b / c + d;
@@ -25,13 +25,13 @@ int main() {
   int res5 = a + b * a - b;
   int res6 = (a + b) / 2;
 
-  printf("Expression of res1 is : %d\n", res1);
-  printf("Expression of res2 is : %d\n", res2);
-  printf("Expression of res3 is : %d\n", res3);
-  printf("Expression of res4 is : %d\n", res4);
-  printf("Expression of res5 is : %d\n", res5);
-  printf("Expression of res6 is : %d\n", res6);
+  printf("Expression of res1 is: %d\n", res1);
+  printf("Expression of res2 is: %d\n", res2);
+  printf("Expression of res3 is: %d\n", res3);
+  printf("Expression of res4 is: %d\n", res4);
+  printf("Expression of res5 is: %d\n", res5);
+  printf("Expression of res6 is: %d\n", res6);
 
   return 0;
+
 }
-// Answer is 5, -3, 2, 0, 1, 1.
