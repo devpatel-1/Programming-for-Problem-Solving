ba9c258370274977e8837080d2280224e6544484
devpatel09092006 <24012011080@gnu.ac.in>
2025-06-06 23:15:27 +0530

    Create 1.c

---
+++ Final Exam QuestionBank/1.c
@@ -0,0 +1,20 @@
+// Write a program to display a multiplication table using a while loop.
+
+#include <stdio.h>
+
+int main () {
+
+   int num, i = 1;
+
+   printf("Enter a number: ");
+   scanf("%d", &num);
+
+   while (i <= 10) {
+        printf("%d x %d = %d\n", num, i, num * i);
+        i++;
+   }
+
+   return 0;
+
+}
+
