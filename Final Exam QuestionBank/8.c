3e06f4ee1ec7641db96fb04728c09cfdc48be43a
devpatel09092006 <24012011080@gnu.ac.in>
2025-06-06 23:15:08 +0530

    Create 8.c

---
+++ Final Exam QuestionBank/8.c
@@ -0,0 +1,16 @@
+// Write a program to find the sum of first 10 natural numbers using a loop.
+
+#include <stdio.h>
+
+int main () {
+
+    int i, sum = 0;
+
+    for (i = 1; i <= 10; i++) {
+        sum = sum + i;
+    }
+
+    printf("Sum of first 10 natural numbers is: %d\n", sum);
+
+    return 0;
+}
