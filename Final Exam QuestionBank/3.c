b3207b6f8df5543131de7139ecdf4b877e6ff36d
devpatel09092006 <24012011080@gnu.ac.in>
2025-06-06 23:15:11 +0530

    Create 3.c

---
+++ Final Exam QuestionBank/3.c
@@ -0,0 +1,23 @@
+// Design a program to accept 10 numbers from the user and print only even
+// numbers
+
+#include <stdio.h> 
+
+int main () {
+
+    int numbers[10], i;
+
+    printf("Enter 10 numbers:\n");
+    for (i = 0; i < 10; i++) {
+        scanf("%d", &numbers[i]);
+    }
+
+    printf("Even numbers are:\n");
+    for (i = 0; i < 10; i++) {
+        if (numbers[i] % 2 == 0) {
+            printf("%d\n", numbers[i]);
+        }
+    }
+
+    return 0;
+}
