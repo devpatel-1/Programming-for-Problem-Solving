e5a7ebbab43b83aff663b3b0ad608b3976a8369b
devpatel09092006 <24012011080@gnu.ac.in>
2025-06-11 08:50:51 +0530

    Update 12.c

--- Final Exam QuestionBank/12.c
+++ Final Exam QuestionBank/12.c
@@ -5,22 +5,22 @@
 
 int main () {
 
-    int num, sum = 0, digit;
+   int num, sum = 0, digit;
 
-    printf("Enter a number: ");
-    scanf("%d", &num);
+   printf("Enter a number: ");
+   scanf("%d", &num);
 
-    while (num != 0) {
+   while (num != 0) {
         digit = num % 10;
         sum = sum + digit;
         num = num / 10;
-    } 
+   }
 
-    printf("Sum of digits is: %d\n", sum);
+   printf("Sum of digit is: %d\n", sum);
 
-    return 0;
+   return 0;
+
 }
-
 
 // Explanation:
 // 	•	The last digit is extracted using num % 10.
