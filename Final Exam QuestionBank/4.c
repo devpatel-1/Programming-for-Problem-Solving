3b9501969c4187d64c0414c1f7c4635899d3a8dd
devpatel09092006 <24012011080@gnu.ac.in>
2025-06-13 09:14:03 +0530

    Update 4.c

--- Final Exam QuestionBank/4.c
+++ Final Exam QuestionBank/4.c
@@ -1,25 +1,26 @@
 // Write a recursive C function to compute the factorial of a number.
 
+
 #include <stdio.h>
 
-int factorial (int n) {
+int factorial(int n) {
 
-    if (n = 0 || n == 1) {
+    if (n == 0 || n == 1) {
         return 1;
     } else {
         return n * factorial(n - 1);
     }
 
-    int main () {
+}
 
-        int num;
+int main () {
 
-        printf("Enter a number: ");
-        scanf("%d", &num);
+    int num;
 
-        printf("Factorial of %d is %d\n", num, factorial(num));
-    }
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    printf("Factorial of %d is %d\n", num, factorial(num));
 
     return 0;
-
 }
