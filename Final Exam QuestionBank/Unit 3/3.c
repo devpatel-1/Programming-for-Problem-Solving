041356d64d33efbc6866c9b051ad28d81a75207f
devpatel09092006 <24012011080@gnu.ac.in>
2025-06-14 10:24:16 +0530

    Update 3.c

--- Final Exam QuestionBank/Unit 3/3.c
+++ Final Exam QuestionBank/Unit 3/3.c
@@ -4,27 +4,26 @@
 
 int main () {
 
-    int arr[3][3], i, j, sum;
+   int arr[3][3], i, j, sum;
 
-    printf("Enter elements of matrix: \n");
-    for (i = 0; i < 3; i++) {
-        for (j = 0; j < 3; j++) {
-            scanf("%d", &arr[i][j]);
-        }
-    }
+   printf("Enter elements of a 3x3 matrix:\n"); 
+   for (i = 0; i < 3; i++) {
+       for (j = 0; j < 3; j++) {
+           scanf("%d", &arr[i][j]);
+       }
+   }
 
-    for (i = 0; i < 3; i++) {
-        sum = 0;
+   for (i = 0; i < 3; i++) {
+        sum = 0
         for (j = 0; j < 3; j++) {
             sum += arr[i][j];
         }
-        printf("Sum of row %d is: %d\n", i + 1, sum);
-    }
+        printf("Sum of row %d is %d\n", i + 1, sum);
+   }
 
     return 0;
 
 }
-
 
 // Explanation:
 // 	•	We first take input for a 3×3 matrix.
